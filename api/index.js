51dfcd3c947fd6185b2c3f9d55295de24d2cfac8
Umar Farooq <umarfarooqbcs@gmail.com>
2025-01-12 05:39:09 +0000

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
-import express from 'express'
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+dotenv.config();
+
+//UmarFarooq
+//Umar@1034
+//umarfarooqbcs
+//NfuWAmCvaBRNtDxH
+
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("database connected")
+}).catch((error)=>{
+    console.log(error)
+})
 
 
 const app = express();
